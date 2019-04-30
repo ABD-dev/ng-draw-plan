aa910d5e8819eac072f18f355fdc01b67bfee1b5
Bogdan Dinga <bogdan.dinga@e-spres-oh.com>
2019-04-30 13:10:12 +0300

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit, Renderer2, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'draw-test';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('svg') 
+  private elSvg : ElementRef;
+  private rect: any;
+  drawing = false;
+  private lastPoint = null;
+
+  constructor(private renderer:Renderer2) {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.rect = this.elSvg.nativeElement.getBoundingClientRect();
+    this.elSvg.nativeElement.addEventListener('mousedown', (e) => this.mouseDownEvent(e));
+    this.elSvg.nativeElement.addEventListener('mousemove', (e) => this.mouseOverEvent(e));
+  }
+
+  draw() {
+    this.drawing = !this.drawing;
+    this.lastPoint = null;
+  }
+
+  private mouseDownEvent(event) {
+    if (!this.drawing) {
+      return;
+    }
+    const pt = this.getMousePosition(event);
+    let circle = this.renderer.createElement('circle', 'svg');
+    circle.setAttribute("fill", "none");
+    circle.setAttribute("stroke", "#000");
+    circle.setAttribute("cx", pt.x);
+    circle.setAttribute("cy", pt.y);
+    circle.setAttribute("r", 5);
+    this.renderer.appendChild(this.elSvg.nativeElement, circle);
+
+    if (!this.lastPoint) {
+      this.lastPoint = {...pt};
+    } else {
+      const strPath = `M${this.lastPoint.x} ${this.lastPoint.y} L${pt.x} ${pt.y}`;
+      let path = this.renderer.createElement('path', 'svg');
+      path.setAttribute("stroke", '#000');
+      path.setAttribute("d", strPath);
+      this.renderer.appendChild(this.elSvg.nativeElement, path);
+      this.lastPoint = {...pt};
+    }
+  }
+
+  private mouseOverEvent(event) {
+    if (!this.drawing) {
+      return;
+    }
+    // <path d="M171 81 L175 217" stroke="#000" />
+    // console.log(event);
+  }
+
+  private getMousePosition(e) {
+    return {
+        x: e.pageX - this.rect.left,
+        y: e.pageY - this.rect.top
+    }
+  };
 }
